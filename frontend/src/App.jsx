717f9ce051a3c52cbba5e96886a4f7955703457c
Huy Nguyen <ngochuy03062004@gmail.com>
2025-05-27 16:23:25 +0700

    Login Functionality & Authentication Setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,20 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard'
+import EmployeeDashboard from './pages/EmployeeDashboard'
 
 function App() {
   
 
   return (
-    <div className="text-3xl text-teal-500">Welcome to Employee Management System</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element= {<Navigate to="/admin-dashboard"/>}></Route>
+        <Route path="/login" element= {<Login/>}></Route>
+        <Route path="/admin-dashboard" element= {<AdminDashboard/>}></Route>
+        <Route path="/employee-dashboard" element= {<EmployeeDashboard/>}></Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
+import authRouter from './routes/auth.js'
+import connectToDatabase from './db/db.js'
 
+connectToDatabase()
 const app =express()
-app.use(cor())
+app.use(cors())
 app.use(express.json())
-
+app.use('/api/auth', authRouter)
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port ${process.env.PORT}')
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
